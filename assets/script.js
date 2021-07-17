ff49ef634c39b7e65ef60e5114d46cf673b31d20
dMosca <devinmosca@yahoo.com>
2021-07-16 23:17:01 -0600

    JS mostly outlined

--- assets/script.js
+++ assets/script.js
@@ -18,4 +18,20 @@
 
 // function with add event listener for start button to start the quiz.
 // need countdown timer to start with the quiz --- time left needs to be recorded as highscore 
+// add time reduction function for incorrect answers
 // make an array of objects(questions) with properties (1.correct answer, 2.array of possible answers 3.)
+// store the questions and corresponding correct answers 
+// add event listener for user answer selection so selection can be stored on the local storage
+// creat a function to compare the user selection with the correct answer for that question
+// use for loops to go through question and answer arrays
+// stop the quiz once question array is complete or if the timer is === 0
+// once the game is over user can save their score attached with their initials to be stored on the local storage.
+
+// menu for highscore needs to link to a separate html 
+// ---do the same for change of difficulty and bonus quiz
+
+// personal objectives after criteria is met and if time permits
+// make start button change to "Good Luck!" after click event
+// add at least 2 additional question arrays for different difficulties easy med hard
+// create new q/a arrays to incorporate HTML and CSS fundamentals
+// make high score only include top 5 highest scores, add date stamp
