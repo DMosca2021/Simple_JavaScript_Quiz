735fe534de47ad9692e6729cb89d4e8279ce3dc3
dMosca <devinmosca@yahoo.com>
2021-07-16 22:54:42 -0600

    Started pseudocoding JS

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+// function with add event listener for start button to start the quiz.
+// need countdown timer to start with the quiz --- time left needs to be recorded as highscore 
+// make an array of objects(questions) with properties (1.correct answer, 2.array of possible answers 3.)
